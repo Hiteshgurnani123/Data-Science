0231888e3226b16d7de656e895509047b2d09414
Hiteshgurnani123 <hiteshgurnani100@gmail.com>
2025-08-06 15:28:06 +0530

    exeption handling done

---
+++ python/Exeption Handling/main.py
@@ -0,0 +1,15 @@
+a = int(input("tell your number:-"))
+
+try:
+    print(10/a)
+
+except ZeroDivisionError:
+    print("You cannot divide by zero, please enter a non-zero number."  )
+
+else:
+    print("there is no error")
+
+finally:
+    print('Always run no matter what')
+    
+print("This line will always execute after the try-except block.")
